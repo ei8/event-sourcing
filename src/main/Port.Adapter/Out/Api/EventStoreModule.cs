968fb3d085a8d9991ff4f75e5da3278248b51378
Elmer Roderick Bool <elmer.bool@ei8.works>
2020-04-16 23:55:50 +1000

    Removed Avatar references.

--- src/main/Port.Adapter/Out/Api/EventStoreModule.cs
+++ src/main/Port.Adapter/Out/Api/EventStoreModule.cs
@@ -11,13 +11,13 @@ namespace works.ei8.EventSourcing.Port.Adapter.Out.Api
 {
     public class EventStoreModule : NancyModule
     {
-        public EventStoreModule(IEventStoreApplicationService eventStoreService) : base("/{avatarId}/eventsourcing/eventstore")
+        public EventStoreModule(IEventStoreApplicationService eventStoreService) : base("/eventsourcing/eventstore")
         {
             this.Get("/{aggregateid}", async (parameters) => {
                     int version = 0;
                     if (this.Request.Query.version.HasValue)
                         version = int.Parse(this.Request.Query.version);
-                    var notifs = await eventStoreService.Get(parameters.avatarId, Guid.Parse(parameters.aggregateId), version);
+                    var notifs = await eventStoreService.Get(Guid.Parse(parameters.aggregateId), version);
                     return new TextResponse(JsonConvert.SerializeObject(notifs));
                 }
             );
