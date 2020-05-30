cf8a37099cd9a5e558d4e19d8fbb447c99ced433
elmerbool <35117496+elmerbool@users.noreply.github.com>
2020-05-30 22:29:19 +1000

    Fixes for docker push.

--- src/main/Port.Adapter/Common/Constants.cs
+++ src/main/Port.Adapter/Common/Constants.cs
@@ -5,5 +5,6 @@ namespace ei8.EventSourcing.Port.Adapter.Common
     public struct EnvironmentVariableKeys
     {
         public const string DatabasePath = "DATABASE_PATH";
+        public const string DisplayErrorTraces = "DISPLAY_ERROR_TRACES";
     }
 }
--- src/main/Port.Adapter/Out/Api/CustomBootstrapper.cs
+++ src/main/Port.Adapter/Out/Api/CustomBootstrapper.cs
@@ -4,6 +4,9 @@ using ei8.EventSourcing.Application.EventStores;
 using ei8.EventSourcing.Application.Notifications;
 using ei8.EventSourcing.Port.Adapter.IO.Persistence.Events.SQLite;
 using dmIEventStore = ei8.EventSourcing.Domain.Model.IEventStore;
+using Nancy.Configuration;
+using System;
+using ei8.EventSourcing.Port.Adapter.Common;
 
 namespace ei8.EventSourcing.Port.Adapter.Out.Api
 {
@@ -20,6 +23,12 @@ namespace ei8.EventSourcing.Port.Adapter.Out.Api
             container.Register<dmIEventStore, EventStore>();
             container.Register<INotificationApplicationService, NotificationApplicationService>();
             container.Register<IEventStoreApplicationService, EventStoreApplicationService>();
+        }
+
+        public override void Configure(INancyEnvironment environment)
+        {
+            environment.Tracing(false, bool.Parse(Environment.GetEnvironmentVariable(EnvironmentVariableKeys.DisplayErrorTraces)));
+            base.Configure(environment);
         }
     }
 }
